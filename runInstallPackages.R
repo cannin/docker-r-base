660cdb9eadd75c199c6ca87cfa701e6aa2ac38c5
cannin <cannin@gmail.com>
2017-08-25 07:47:20 -0400

    Added stringr upfront

--- runInstallPackages.R
+++ runInstallPackages.R
@@ -1,7 +1,7 @@
 # Set up to use CRAN
 setRepositories(ind=1:6)
 options(repos="http://cran.rstudio.com/")
-if(!require(devtools)) { install.packages("devtools") }
+install.packages(c("devtools", "stringr"))
 
 source("https://gist.githubusercontent.com/cannin/6b8c68e7db19c4902459/raw/installPackages.R")
 installPackages("r-requirements.txt")
